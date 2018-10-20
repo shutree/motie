6dc1d5cd46d5aedc7fc6344739a2bf2acc774838
dapang <771029485@qq.com>
2018-10-20 09:09:25 +0800

    增加分类路由

--- src/router/index.js
+++ src/router/index.js
@@ -58,6 +58,13 @@ export default new Router({
       path: '/details',
       name: 'details',
       component: Details
+    },
+    {
+      path: '/category',
+      name: 'category',
+      component:{
+        template:`<div>分类页</div>`
+      }
     }
   ]
 })
