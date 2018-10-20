e9950a14c97b9815c74bad730e68d7875a5c1531
dapang <771029485@qq.com>
2018-10-20 08:55:33 +0800

    登录页

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,18 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import Mint from 'mint-ui';
+Vue.use(Mint);
+import 'mint-ui/lib/style.css';
+import axios from 'axios';
+Vue.use(axios);
+Vue.prototype.$axios = axios;
+
+// loading
+import { Indicator } from 'mint-ui';
+Vue.prototype.$loading = Indicator;
+
 import './sass/base.scss';
 export default {
   name: 'App'
--- src/page/Login.vue
+++ src/page/Login.vue
@@ -1,10 +1,16 @@
 <template>
 	<div class="login" >
-		这是登录页
+		<mt-header title="登录页面" :style='{background:"#000"}'>
+		  <router-link to="/" slot="left">
+		    <mt-button icon="back">back</mt-button>
+		  </router-link>
+		  <mt-button icon="more" slot="right"></mt-button>
+		</mt-header>
 	</div>
 </template>
 
 <script>
+import { Header } from 'mint-ui';
 	export default {
 		data(){
 			return {
