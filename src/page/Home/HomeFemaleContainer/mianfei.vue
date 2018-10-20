bcd2d44d51c9608604c3a9c15a9f9a4c666e4b8b
zhouyang <602482790@qq.com>
2018-10-20 22:30:29 +0800

    准备拉取mn_dev 进行合并

--- src/page/Home/HomeFemaleContainer/mianfei.vue
+++ src/page/Home/HomeFemaleContainer/mianfei.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Collection">
+  <div class="mianfei">
     
   </div>
 </template>
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <style lang="scss">
-  .Collection{
+  .mianfei{
     
   }
 </style>
--- src/page/Home/HomeFemaleContainer/wanben.vue
+++ src/page/Home/HomeFemaleContainer/wanben.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Collection">
+  <div class="wanben">
     
   </div>
 </template>
@@ -12,7 +12,7 @@ export default {
 </script>
 
 <style lang="scss">
-  .Collection{
+  .wanben{
     
   }
 </style>
